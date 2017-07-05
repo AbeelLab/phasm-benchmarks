42607be57bd60a7147fbe880dcaba1af6ea093dd
Lucas van Dijk <info@return1.net>
2017-07-05 15:52:19 +0200

    Add SLURM submit script

---
+++ phase_all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --partition=bigmem
+#SBATCH --qos=bigmem
+#SBATCH --time=23:59:59
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32G
+#SBATCH --mail-type=ALL
+#SBATCH --work-dir=/tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/phasm-benchmarks
+
+mkdir -p /tmp/lrvandijk/phasm-benchmarks
+cp -r * /tmp/lrvandijk/phasm-benchmarks
+cd /tmp/lrvandijk/phasm-benchmarks
+
+srun snakemake --cores=16 all
+
+cp -r reads/* assemblies/* /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/phasm-benchmarks
