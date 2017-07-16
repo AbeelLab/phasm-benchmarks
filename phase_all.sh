e27573d5d4e0fce5e1acf3b84d0ecf95b0be03f9
Lucas van Dijk <info@lucasvandijk.nl>
2017-07-16 19:00:26 +0200

    Update DALIGNER settings and SLURM script

--- phase_all.sh
+++ phase_all.sh
@@ -4,15 +4,16 @@
 #SBATCH --time=23:59:59
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH --mail-type=ALL
 #SBATCH --workdir=/tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/phasm-benchmarks
 
-rm -rf /tmp/lrvandijk/phasm-benchmarks
+srun --cpus-per-task=1 rm -rf /tmp/lrvandijk/phasm-benchmarks
 mkdir -p /tmp/lrvandijk/phasm-benchmarks
-cp -r * /tmp/lrvandijk/phasm-benchmarks
+srun --cpus-per-task=1 cp -r * /tmp/lrvandijk/phasm-benchmarks
 cd /tmp/lrvandijk/phasm-benchmarks
 
 srun snakemake --cores=16 all
 
-cp -r reads/ assemblies/ /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/phasm-benchmarks
+srun --cpus-per-task=1 rm -rf /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/last_benchmark/*
+srun --cpus-per-task=1 cp -r * /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/last_benchmark
