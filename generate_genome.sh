fa708d3858aa2bedaaa726a06585bc3b0e91794a
Lucas van Dijk <info@return1.net>
2017-07-20 11:09:34 +0200

    Do not create translocation

--- generate_genome.sh
+++ generate_genome.sh
@@ -13,11 +13,4 @@ OUTPUT_DIR="${2}"
 mkdir -p ${OUTPUT_DIR}/haplotypes
 
 aneusim haplogen -r -s ${SPEC_FILE} ${BASE_GENOME} ${OUTPUT_DIR}/haplotypes
-
-# Translocation of length 50000
-cp "${SCRIPT_DIR}/base/haplotypes/BK006941.2.copy0.fasta" "${OUTPUT_DIR}/haplotypes/temp.fasta"
-aneusim translocate -m 0 -l 50000 50000 --in-place \
-    "${OUTPUT_DIR}/haplotypes/temp.fasta" ${OUTPUT_DIR}/haplotypes/*.copy0.fasta
-rm "${OUTPUT_DIR}/haplotypes/temp.fasta"
-
 cat ${OUTPUT_DIR}/haplotypes/*.fasta > "${OUTPUT_DIR}/genome.fasta"
