5ab6a528f2290b6f889848fbbb543b4bbb66829f
Lucas van Dijk <info@lucasvandijk.nl>
2017-07-24 12:43:27 +0200

    Update sbatch script

--- force_run_all.sh
+++ force_run_all.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 #SBATCH --partition=bigmem
 #SBATCH --qos=bigmem
-#SBATCH --time=23:59:59
+#SBATCH --time=6-23:59:59
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 #SBATCH --mail-type=ALL
 #SBATCH --workdir=/tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/phasm-benchmarks
 
-srun --cpus-per-task=1 rm -rf /tmp/lrvandijk/phasm-benchmarks
+rm -rf /tmp/lrvandijk/phasm-benchmarks
 mkdir -p /tmp/lrvandijk/phasm-benchmarks
-srun --cpus-per-task=1 cp -r * /tmp/lrvandijk/phasm-benchmarks
+cp -r * /tmp/lrvandijk/phasm-benchmarks
 cd /tmp/lrvandijk/phasm-benchmarks
 
-srun snakemake --cores=16 all
+snakemake --keep-going --cores=16 all_genomes
+snakemake --keep-going --cores=16 all_errorfree_readsets
+snakemake --keep-going --cores=16 all
 
-srun --cpus-per-task=1 rm -rf /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/last_benchmark/*
-srun --cpus-per-task=1 cp -r * /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/last_benchmark
+rm -rf /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/last_benchmark/*
+cp -r * /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/last_benchmark
