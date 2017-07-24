1e504990358466a5a1ce47b24d6e7638934826c0
Lucas van Dijk <info@return1.net>
2017-07-24 13:03:41 +0200

    Add script to create cluster jobs

---
+++ create_sbatch_scripts.py
@@ -0,0 +1,31 @@
+import yaml
+
+with open("config.yml") as f:
+    config = yaml.load(f)
+
+TEMPLATE = """
+#!/bin/sh
+#SBATCH --partition=bigmem
+#SBATCH --qos=bigmem
+#SBATCH --time=3-23:59:59
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --mail-type=ALL
+#SBATCH --workdir=/tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/phasm-benchmarks
+
+rm -rf /tmp/lrvandijk/assemblies/{assembly}
+mkdir -p /tmp/lrvandijk/assemblies/{assembly}
+cp -r * /tmp/lrvandijk/assemblies/{assembly}
+cd /tmp/lrvandijk/assemblies/{assembly}
+
+snakemake --keep-going --cores=8 assemblies/{assembly}/{assembly}.fasta
+
+mkdir -p /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/assemblies/{assembly}
+cp -r assemblies/{assembly}/* /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/assemblies/{assembly}
+"""
+
+for assembly in config['assemblies'].keys():
+    fname = "phasm_{}.sh".format(assembly)
+    with open(fname, "w") as f:
+        f.write(TEMPLATE.format(assembly=assembly).lstrip())
