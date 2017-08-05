f22a5ed38c979c27ab2f4592e5c743e4e8d9abc4
Lucas van Dijk <info@lucasvandijk.nl>
2017-08-05 20:29:46 +0200

    Also copy reads to network folder

--- create_sbatch_scripts.py
+++ create_sbatch_scripts.py
@@ -9,7 +9,7 @@ TEMPLATE = """
 #SBATCH --qos=bigmem
 #SBATCH --time=3-23:59:59
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=2
 #SBATCH --mem=64G
 #SBATCH --mail-type=ALL
 #SBATCH --workdir=/tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/phasm-benchmarks
@@ -18,15 +18,23 @@ rm -rf /tmp/lrvandijk/assemblies/{assembly}
 mkdir -p /tmp/lrvandijk/assemblies/{assembly}
 cp -r * /tmp/lrvandijk/assemblies/{assembly}
 cd /tmp/lrvandijk/assemblies/{assembly}
+rm -rf .snakemake/
 touch genomes/*/genome.fasta
 
-snakemake --keep-going --cores=8 -f \\
+sleep 1
+
+snakemake --keep-going --cores=2 -f \\
     reads/error_free/{reads}.fasta
-snakemake --keep-going --cores=8 -f \\
+
+sleep 2
+
+snakemake --keep-going --cores=2 -f \\
     assemblies/{assembly}/04_phase/{assembly}.fasta
 
-mkdir -p /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/assemblies/{assembly}
+mkdir -p /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/assemblies/{assembly}/00_reads
 cp -r assemblies/{assembly}/* /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/assemblies/{assembly}
+cp reads/error_free/{reads}.fasta reads/error_free/{reads}.fasta.fai reads/error_free/{reads}.fasta.json \\
+    /tudelft.net/staff-bulk/ewi/insy/DBL/lrvandijk/assemblies/{assembly}/00_reads
 """
 
 for assembly in config['assemblies'].keys():
